98bb280fd16dc18759b45af147468b66d320b919
Максим Тутуков <m.tutukov@kokoc.tech>
2023-04-18 17:52:11 +0300

    feat(master): Добавляет proxy

---
+++ src/scripts/structural/proxy.ts
@@ -0,0 +1,76 @@
+{
+  interface Subject {
+    request(): void;
+  }
+
+  class RealSubject implements Subject {
+    public request(): void {
+      console.log('proxy: RealSubject - Handling request.');
+    }
+  }
+
+  class Proxy implements Subject {
+    private realSubject: RealSubject;
+
+    constructor(realSubject?: RealSubject) {
+      this.realSubject = realSubject || new RealSubject();
+    }
+
+    public request(): void {
+      this.realSubject.request();
+      this.logAccess();
+    }
+
+    private logAccess(): void {
+      console.log('proxy: log');
+    }
+  }
+
+  const realSubject = new RealSubject();
+  realSubject.request();
+
+  const proxy = new Proxy(realSubject);
+  proxy.request();
+
+  // Пример 1
+  interface ICarAccess {
+    open: () => void;
+    close: () => void;
+  }
+
+  class CarAccess implements ICarAccess {
+    open() {
+      console.log('proxy: CarAccess - open');
+    }
+
+    close() {
+      console.log('proxy: CarAccess - close');
+    }
+  }
+
+  class SecuritySystem implements ICarAccess {
+    private door: CarAccess;
+
+    constructor(door?: CarAccess) {
+      this.door = door || new CarAccess();
+    }
+
+    open() {
+      this.authenticated();
+      this.door.open();
+    }
+
+    authenticated() {
+      console.log('proxy: SecuritySystem - authenticated');
+    }
+
+    close() {
+      this.door.close();
+    }
+  }
+
+  const securitySystem = new SecuritySystem();
+  securitySystem.open();
+
+  console.log('====================================');
+}
