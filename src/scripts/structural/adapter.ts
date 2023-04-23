d205c03cb69f81bc3f41cdd55f8dd28174266f13
Максим Тутуков <m.tutukov@kokoc.tech>
2023-04-23 21:58:43 +0300

    feat(master): Добавляет пример для adapter

--- src/scripts/structural/adapter.ts
+++ src/scripts/structural/adapter.ts
@@ -34,5 +34,46 @@
   const adapter = new Adapter(adapted);
   console.log('adapter: specific - ', adapter.request());
 
+  // Пример 1
+  class OldCalc {
+    operations(t1: number, t2: number, operation: 'add' | 'sub') {
+      switch (operation) {
+        case 'add': return t1 + t2;
+        case 'sub': return t1 - t2;
+        default: return NaN;
+      }
+    }
+  }
+
+  class NewCalc {
+    add(t1: number, t2: number) {
+      return t1 + t2;
+    }
+
+    sub(t1: number, t2: number) {
+      return t1 - t2;
+    }
+  }
+
+  class CalcAdapter extends OldCalc {
+    private calc: NewCalc;
+
+    constructor() {
+      super();
+      this.calc = new NewCalc();
+    }
+
+    operations(t1: number, t2: number, operation: 'add' | 'sub') {
+      switch (operation) {
+        case 'add': return this.calc.add(t1, t2);
+        case 'sub': return this.calc.sub(t1, t2);
+        default: return NaN;
+      }
+    }
+  }
+
+  const calc = new CalcAdapter();
+  console.log(`adapter: ${calc.operations(2, 2, 'add')}`);
+
   console.log('====================================');
 }
